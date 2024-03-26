cb7350440019aeec97a98252d889c7d0848846d8
JYH <dydgusc66@naver.com>
2024-03-26 10:37:58 +0900

    first commit

---
+++ MyReact/myreact01/src/App02.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Menu from "./components01/Menu";
+import Footer from "./components/Footer";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>* 요리 백과*</h2>
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
